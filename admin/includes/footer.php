ba9ed90ada9891feeee5f250511cf58656d8606f
Vigyani23 <67911220+Vigyani23@users.noreply.github.com>
2020-12-17 17:30:42 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <body style="background-color:black">
+                <div class="col-md-12">
+                   Library Management System <br>Designed by : Rishikesh Patil</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
